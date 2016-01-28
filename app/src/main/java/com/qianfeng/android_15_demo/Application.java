03d35c2a6373e6492e79e5b47e0c7e98dc79fe17
dupengfei <990334459@qq.com>
2016-01-28 22:11:19 +0800

    test

--- app/src/main/java/com/qianfeng/android_15_demo/Application.java
+++ app/src/main/java/com/qianfeng/android_15_demo/Application.java
@@ -3,4 +3,7 @@ package com.qianfeng.android_15_demo;
 /**
  * Created by dupengfei on 16/1/25.
  */
-public class Application extends com.activeandroid.app.Application {}
+public class Application extends com.activeandroid.app.Application {
+
+    //test
+}
